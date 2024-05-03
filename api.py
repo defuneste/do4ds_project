582f8c59becaa6bc431e47713653e9b8232205cf
John Hall <john.hall@ruralinnovation.us>
2024-05-03 13:01:13 -0400

    update requirements and notebooks to use duckdb

--- api.py
+++ api.py
@@ -6,18 +6,18 @@ from vetiver import VetiverModel
 from vetiver import VetiverAPI
 
 # This is how you would reload the model from disk...
-b = board_folder('data/model', allow_pickle_read = True)
+b = board_folder('data/model', allow_pickle_read=True)
 v = VetiverModel.from_pin(b, 'penguin_model')
 
 # ... however VertiverAPI also uses the model inputs to define params from the prototype
 df = penguins.load_penguins().dropna()
 df.head(3)
-X = get_dummies(df[['bill_length_mm', 'species', 'sex']], drop_first = True)
+X = get_dummies(df[['bill_length_mm', 'species', 'sex']], drop_first=True)
 y = df['body_mass_g']
 
 model = LinearRegression().fit(X, y)
 
-v = VetiverModel(model, model_name = "penguin_model", prototype_data = X)
+v = VetiverModel(model, model_name="penguin_model", prototype_data=X)
 
-app = VetiverAPI(v, check_prototype = True)
-app.run(port = 8000)
+app = VetiverAPI(v, check_prototype=True)
+app.run(port=8000)
