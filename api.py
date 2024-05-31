fa7f8e4b0f74a227727c50011ad88974f3e72af8
John Hall <john.hall@ruralinnovation.us>
2024-05-31 08:54:07 -0400

    fix pins board output for python api

--- api.py
+++ api.py
@@ -5,6 +5,17 @@ from pins import board_folder
 from vetiver import VetiverModel
 from vetiver import VetiverAPI
 
+import logging
+
+# Configure the log object
+logging.basicConfig(
+    format='%(asctime)s - %(message)s',
+    level=logging.INFO
+)
+
+# Log app start
+logging.info("API Started")
+
 # This is how you would reload the model from disk...
 b = board_folder('data/model', allow_pickle_read=True)
 v = VetiverModel.from_pin(b, 'penguin_model')
